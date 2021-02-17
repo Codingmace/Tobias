a35252835fdee760e9fc5ab16c32093976b0970a
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-02-17 11:04:59 -0600

    Fixes to setup scripte.

--- Tobias Take1/setup.py
+++ Tobias Take1/setup.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+
 import os
+import sys
+import configparser
 
 def intro():
     print("Hello and welcome to the setup of Tobias")
@@ -16,9 +20,9 @@ def keys():
         return
 
     rapidValid = False
-    rapidApiSelect = input("Do you have a rapidAPIKey (yes/no)? ")
+    rapidApiSelect = input("Do you have a rapidAPIKey (yes/no)? ").strip().lower()
     rapidApi = "3f9620697cmsh4d8a1a910032d97p15547bjsn7f4d1d89819c"
-    if "yes" in rapidApiSelect.lower():
+    if rapidApiSelect == "yes" or rapidApiSelect == "y":
         while not rapidValid:
             rapidApi = input("Please input your rapidApiKey now: ").strip()
             if len(rapidApi) == 50:
@@ -33,9 +37,9 @@ def keys():
         print("That sucks. Using Default Key and Limiting the API able to use")
 
     dncValid = False
-    dncValidSelect = input("Do you have a doNotCall ApiKey (yes/no)? ").strip()
+    dncValidSelect = input("Do you have a doNotCall ApiKey (yes/no)? ").strip().lower()
     dncApi = ""
-    if "yes" in dncValidSelect.lower():
+    if dncValidSelect == "yes" or dncValidSelect == "y":
         while not dncValid:
             dncApi = input("Please input your doNotCallApiKey now: ").strip()
             if len(dncApi) == 40:
@@ -53,19 +57,19 @@ def keys():
     f = open("API/variables.py", "w")
     
     variableInfo = "rapidApiKey = \"" + rapidApi + "\"\ndncApiKey = \"" + dncApi + "\"\n"
-    variableInfo2 = 'weatherInfo = {"language" : "english", "lang" : "en", "units": "e", "callback": "test", "mode":"JSON", "types": "link, accurate", "measurement":"imperial", "count": "10"} # Can change measurement to metric")'
+    variableInfo2 = 'weatherInfo = {"language" : "english", "lang" : "en", "units": "e", "callback": "test", "mode":"JSON", "types": "link, accurate", "measurement":"imperial", "count": "10"} # Can change measurement to metric\n'
     f.write(variableInfo)
     f.write(variableInfo2)
-    enableSpeech = input("Would you like to enable speech? ")
-    if "yes" in enableSpeech:
-        f.write("speechEnabled=True")
+    enableSpeech = input("Would you like to enable speech? ").strip().lower()
+    if enableSpeech == "yes" or enableSpeech == "y":
+        f.write("speechEnabled = True\n")
     else:
-        f.write("speechEnabled=False")
-    faceRecog = input("Would you like to enable Facial Recognition? ")
-    if "yes" in faceRecog:
-        f.write("faceEnabled=True")
+        f.write("speechEnabled = False\n")
+    faceRecog = input("Would you like to enable Facial Recognition? ").strip().lower()
+    if faceRecog == "yes" or faceRecog == "y":
+        f.write("faceEnabled = True\n")
     else:
-        f.write("faceEnabled=False")
+        f.write("faceEnabled = False\n")
     f.flush()
     f.close()
 
@@ -75,41 +79,39 @@ def printLogo():
     for line in lines:
         print(line.strip())
     f.close()
-#    input()
 
 def createRequirements():
     print("Here we select what we want to add to the requirements. Some people don't want to dowload everything")
     print("Later on that may become a huge file and nobody wants to waste space")
     print("Options:\n1.All\n2.Minimal\n3.Basic\n4.Speech\n5.Google")
     selection = int(input("Input your selection: "))
-    f = open("Data/allRequirements.txt", "r")
+    allReqs = configparser.ConfigParser()
+    allReqs.read("Data/allRequirements.ini")
     req = open("requirements.txt", "w")
-    lines = f.readlines()
-    f.close()
-    minimal = [2, 3, 13, 18, 20, 35, 38, 44]
-    basic = [0, 14, 17, 23, 24, 25, 27, 29, 30, 31, 40, 45] # Minimal
-    speech = [4, 28, 32, 34, 41] # Basic
-    google = [1, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, 19, 21, 22, 26, 33, 36, 37, 39, 42, 43] # Basic
     downloading = []
     if selection == 1: # Just copy the file
-        for line in lines:
-            req.write(line)
+        for section in allReqs:
+            for line in allReqs[section]:
+                req.write(line + "==" + allReqs[section][line].strip('\"') + "\n")
     if selection >= 2 and selection <= 5: # Incrementally figure it out
-        downloading += minimal
+        downloading.append("minimal")
         if selection >= 3:
-            downloading += basic
+            downloading.append("basic")
             if selection == 4:
-                downloading += speech
+                downloading.append("speech")
+                if sys.platform == "win32":
+                    downloading.append("speech-win32")
             if selection == 5:
-                downloading += google
-        for d in downloading:
-            try:
-                req.write(lines[d])
-            except:
-                print("There was an issue. Flushing the file and closing it.")
-                req.flush()
-                req.close()
-                return
+                downloading.append("google")
+        for section in downloading:
+            for line in allReqs[section]:
+                try:
+                    req.write(line + "==" + allReqs[section][line].strip('\"') + "\n")
+                except:
+                    print("There was an issue. Flushing the file and closing it.")
+                    req.flush()
+                    req.close()
+                    return
     req.flush()
     req.close()
     print("Ok we have written the requirements file. Now just run 'pip install -r requirements.txt'")
